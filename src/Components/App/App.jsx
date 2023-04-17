a7107e5673135801ddf8c363e1fb8d6187a22490
AlexPacemaker <118269766+AlexPacemaker@users.noreply.github.com>
2023-04-17 15:14:36 +0300

    Main added to App

---
+++ src/Components/App/App.jsx
@@ -0,0 +1,12 @@
+import styles from "./App.module.scss";
+import Main from "../Main/Main";
+
+function App() {
+  return (
+    <div className={styles.container}>
+      <Main />
+    </div>
+  );
+}
+
+export default App;

05874c0dbdb21e434ff846417f9bc0eedf34c7e2
AlexPacemaker <118269766+AlexPacemaker@users.noreply.github.com>
2023-06-01 12:26:20 +0300

    tsconfig added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./Components/App/App.tsx";
+import App from "./Components/App/App";
 import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";

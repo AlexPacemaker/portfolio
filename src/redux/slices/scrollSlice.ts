70eed20601d6f8999927d2fd84c9664ecd76dae0
AlexPacemaker <118269766+AlexPacemaker@users.noreply.github.com>
2023-06-01 13:56:52 +0300

    upd

---
+++ src/redux/slices/scrollSlice.ts
@@ -0,0 +1,24 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { RootState } from "../store";
+
+interface ScrollState {
+  visible: boolean;
+}
+
+const initialState: ScrollState = {
+  visible: false,
+};
+
+export const scrollSlice = createSlice({
+  name: "scroll",
+  initialState,
+  reducers: {
+    setVisible: (state, action: PayloadAction<boolean>) => {
+      state.visible = action.payload;
+    },
+  },
+});
+
+export const selectScrollData = (state: RootState) => state.scroll.visible;
+export const { setVisible } = scrollSlice.actions;
+export default scrollSlice.reducer;

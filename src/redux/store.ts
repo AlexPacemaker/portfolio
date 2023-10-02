ec94cae23b926aec166010618263ca419cbf7557
AlexPacemaker <118269766+AlexPacemaker@users.noreply.github.com>
2023-10-02 19:27:07 +0300

    upd

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,6 +1,6 @@
 // Эта функция создает Redux-хранилище с помощью redux-toolkit и настраивает его для использования с reducers
 import { configureStore } from "@reduxjs/toolkit";
-import { useDispatch } from "react-redux";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import mainSlice from "./slices/mainSlice";
 import bioSlice from "./slices/bioSlice";
 import mainSkillsSlice from "./slices/mainSkillsSlice";
@@ -18,3 +18,4 @@ export const store = configureStore({
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;

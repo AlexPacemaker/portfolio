8eb80f71897f46d7171712b9952d4d4c23ee0551
AlexPacemaker <118269766+AlexPacemaker@users.noreply.github.com>
2023-10-03 13:56:36 +0300

    upd

---
+++ src/Components/About/Skills/OtherSkills/OtherSkills.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
+import { API_URL4 } from "../../../../config";
+import OtherSkillCard from "./OtherSkillCard/OtherSkillCard";
+import styles from "./OtheSkills.module.scss";
+import { setLoading, setOtherSkills } from "../../../../redux/slices/otherSkillsSlice";
+import { RootState } from "../../../../redux/store";
+
+//получаем данные с бэками и отрисовываем карточки
+const OtherSkills = () => {
+  const { otherSkills, loading } = useSelector((state: RootState) => state.otherSkillsSlice);
+  const dispatch = useDispatch();
+
+  React.useEffect(() => {
+    (async () => {
+      if (typeof API_URL4 === "string") {
+        await axios.get(API_URL4).then((res) => {
+          dispatch(setOtherSkills(res.data));
+          dispatch(setLoading(false));
+        });
+      }
+    })();
+    //eslint-disable-next-line
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      {loading ? (
+        <h2>Loading...</h2>
+      ) : (
+        otherSkills.map((otherSkill) => <OtherSkillCard key={otherSkill.id} {...otherSkill} />)
+      )}
+    </div>
+  );
+};
+
+export default OtherSkills;

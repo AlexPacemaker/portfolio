e061be53a4c29540783f5c949154c09aad3110fa
AlexPacemaker <118269766+AlexPacemaker@users.noreply.github.com>
2023-10-03 14:23:54 +0300

    upd

--- src/Components/Outlets/MainOutlet.tsx
+++ src/Components/Outlets/MainOutlet.tsx
@@ -3,7 +3,7 @@ import Header from "../Header/Header";
 import { Outlet } from "react-router-dom";
 
 //определение компонента с аутлетом
-const MainOutlet: React.FC = () => {
+const MainOutlet = (): JSX.Element => {
   return (
     <div>
       <Header />

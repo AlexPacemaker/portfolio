99b0f454ad65302bb6fc01166ffd7967e7275db1
AlexPacemaker <118269766+AlexPacemaker@users.noreply.github.com>
2023-10-03 14:23:06 +0300

    upd

--- src/Components/NotFound/NotFound.tsx
+++ src/Components/NotFound/NotFound.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import styles from "./NotFound.module.scss";
 
 //компонент возвращает страницу с jsx разметкой, если такой страницы на сайте нет
-const NotFound: React.FC = () => {
+const NotFound = (): JSX.Element => {
   return (
     <div className={styles.container}>
       <span>😕</span>
